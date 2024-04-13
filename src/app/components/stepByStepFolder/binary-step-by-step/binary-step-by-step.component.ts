3435cca813868e5c4d5ca018251fc0fae48fa6de
tma19993 <tmazzurek@gmail.com>
2024-04-14 01:39:23 +0200

    add step by step

--- src/app/components/stepByStepFolder/binary-step-by-step/binary-step-by-step.component.ts
+++ src/app/components/stepByStepFolder/binary-step-by-step/binary-step-by-step.component.ts
@@ -1,24 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { TypeOfCoding } from 'src/app/constants';
 import { CodingService } from 'src/app/services';
+import { BinaryCodingService } from 'src/app/services/coding-services';
 import { Combination } from 'src/app/types';
 
 @Component({
   selector: 'app-binary-step-by-step',
   templateUrl: './binary-step-by-step.component.html',
-  styleUrl: './binary-step-by-step.component.scss'
+  styleUrl: './binary-step-by-step.component.scss',
 })
 export class BinaryStepByStepComponent implements OnInit {
   public combiantion: Combination;
   protected readonly TypeOfCoding: any = TypeOfCoding;
 
   constructor(
-    public codingService: CodingService
+    public codingService: CodingService,
+    public binaryCodingService: BinaryCodingService
   ) {}
   public ngOnInit(): void {
     if (this.codingService.combinationCoding) {
       this.combiantion = this.codingService.combinationCoding;
     }
-
+  }
+  public splitDescription(theString: string, bcd?: boolean): string[] {
+    if (bcd) {
+      return theString.match(/.{1,4}/g) as string[];
+    }
+    else return theString.split('');
+  }
+ public convertFrom7segmentToNumber(message: string): string[]{
+    let splittedStrings: string[] = [];
+      for (let i = 0; i < message.length; i += 7) {
+        splittedStrings.push(message.substr(i, 7));
+      }
+      return splittedStrings;
+  }
+  public convertToBinary(theString: string): string {
+    return Number(theString).toString(2);
+  }
+  public convertToDecimal(theString: string,sevenSegment?: boolean,index? :number): string {
+    return parseInt(theString, 2).toString();
+  }
+  public convertToNumber(theString: string): number {
+    return Number(theString);
   }
 }
--- src/app/services/coding-services/binary-coding.service.ts
+++ src/app/services/coding-services/binary-coding.service.ts
@@ -1,22 +1,22 @@
-import { Injectable } from '@angular/core';
-import { TypeOfCoding } from 'src/app/constants';
-import { Combination } from 'src/app/types';
+import { Injectable } from "@angular/core";
+import { TypeOfCoding } from "src/app/constants";
+import { Combination } from "src/app/types";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class BinaryCodingService {
-  private patterns = [
-    '1111110', // 0
-    '0110000', // 1
-    '1101101', // 2
-    '1111001', // 3
-    '0110011', // 4
-    '1011011', // 5
-    '1011111', // 6
-    '1110000', // 7
-    '1111111', // 8
-    '1111011', // 9
+  private patterns:string[] = [
+    "1111110", // 0
+    "0110000", // 1
+    "1101101", // 2
+    "1111001", // 3
+    "0110011", // 4
+    "1011011", // 5
+    "1011111", // 6
+    "1110000", // 7
+    "1111111", // 8
+    "1111011", // 9
   ];
   constructor() {}
 
@@ -53,27 +53,42 @@ export class BinaryCodingService {
     ) {
       return this.sevenSegmentToNumber(input);
     } else {
-      throw new Error('Unsupported conversion combination');
+      throw new Error("Unsupported conversion combination");
     }
+  }
+  public binaryToGraySingleChar(prevBit: string, currentBit: string):string{
+    return prevBit === currentBit ? "0" : "1";
+  }
+
+  public grayToBinarySingleChar(prevBit: string, currentBit: string):string{
+    return prevBit === "0" ? currentBit : currentBit === "1" ? "0" : "1";
+  }
+
+  public numberToSevenSegmentSingleChar(i: number):string{
+    return this.patterns[i]
+  }
+  
+  public sevenSegmentToNumberSingleChar(message: string): number {
+    return this.patterns.indexOf(message);
   }
 
   private binaryToGray(message: string): string {
-    let result = '';
-    let prevBit = '0';
+    let result = "";
+    let prevBit = "0";
     for (let i = 0; i < message.length; i++) {
       const currentBit = message[i];
-      result += prevBit === currentBit ? '0' : '1';
+      result += this.binaryToGraySingleChar(prevBit,currentBit);
       prevBit = currentBit;
     }
     return result;
   }
 
   private grayToBinary(message: string): string {
-    let result = '';
-    let prevBit = '0';
+    let result = "";
+    let prevBit = "0";
     for (let i = 0; i < message.length; i++) {
       const currentBit = message[i];
-      result += prevBit === '0' ? currentBit : currentBit === '1' ? '0' : '1';
+      result +=  this.grayToBinarySingleChar(prevBit,currentBit);
       prevBit = result[i];
     }
     return result;
@@ -82,50 +97,50 @@ export class BinaryCodingService {
   private BCDToNumber(message: string): string {
     const groups = message.match(/.{1,4}/g);
     if (!groups) {
-      throw new Error('Invalid binary input');
+      throw new Error("Invalid binary input");
     }
     const bcdArray = groups.map((group) => parseInt(group, 2).toString(10));
-    return bcdArray.join('');
+    return bcdArray.join("");
   }
 
   private numberToBCD(message: string): string {
-    const digits = message.split('');
+    const digits = message.split("");
     const binaryArray = digits.map((digit) =>
-      parseInt(digit, 10).toString(2).padStart(4, '0')
+      parseInt(digit, 10).toString(2).padStart(4, "0")
     );
-    return binaryArray.join(' ');
+    return binaryArray.join(" ");
   }
 
   private numberToSevenSegment(message: string): string {
     const number = Number(message);
     if (number >= 0 && number <= 9) {
-      return this.patterns[number];
+      return this.numberToSevenSegmentSingleChar(number);
     } else if (number >= 10) {
       let numbersArray: string[] = [];
       for (let i = 0; i < message.length; i++) {
-        numbersArray.push(this.patterns[i]);
+        numbersArray.push(this.numberToSevenSegmentSingleChar(i));
       }
-      return numbersArray.join(' ');
+      return numbersArray.join(" ");
     } else {
-      return 'Invalid decimal input';
+      return "Invalid decimal input";
     }
   }
 
   private sevenSegmentToNumber(message: string): string {
-    if (message.length <= 7) {
-      return 'Invalid seven-segment input';
+    if (message.length < 7) {
+      return "Invalid seven-segment input";
     }
-    const index = this.patterns.indexOf(message);
+
     if (message.length == 7) {
-      return index.toString();
-    } else if (message.length % 7 == 0) {
+      return this.patterns.indexOf(message).toString();
+    } else if ( message.length % 7 == 0) {
       let splittedStrings: string[] = [];
       for (let i = 0; i < message.length; i += 7) {
-        splittedStrings.push(message.substr(i, 7));
+        splittedStrings.push(this.patterns.indexOf(message.substr(i, 7)).toString());
       }
-      return splittedStrings.join('');
+      return splittedStrings.join("");
     } else {
-      return 'Invalid seven-segment input';
+      return "Invalid seven-segment input";
     }
   }
 }
