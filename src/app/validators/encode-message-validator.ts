a8a1fb1139316b7db6a9b1d5a42a679531f4d828
tma19993 <tmazzurek@gmail.com>
2024-04-26 09:28:10 +0200

    additoinal fix to validation

--- src/app/validators/encode-message-validator.ts
+++ src/app/validators/encode-message-validator.ts
@@ -7,9 +7,9 @@ export function encodeMessageValidator(type: string, codedValue?:string): Valida
     if (type === TypeOfCoding.binary || type == TypeOfCoding.gray) {
       pattern = /^[0-1]{1,}$/;
     } else if (type === TypeOfCoding.number && codedValue == TypeOfCoding.sevenSegment) {
-      pattern = /^\d$/;
+      pattern = /^[0-9]$/;
     } else if ((type === TypeOfCoding.number && codedValue != TypeOfCoding.sevenSegment) ||  type === TypeOfCoding.ascii || type === TypeOfCoding.iso8859 || type === TypeOfCoding.utf8) {
-      pattern = /^\d+$/;
+      pattern = /^[0-9]+$/;
     } else if (type === TypeOfCoding.text) {
       pattern = /^[\s\S]*$/;
     }  else if (type === TypeOfCoding.sevenSegment) {
